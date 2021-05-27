fc70abf820daaaf9c49c25bfbb6205b3c968bc0d
zoran <zorani@gmail.com>
2021-05-27 01:08:59 +0100

    save

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from distutils.core import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "README.html"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="cloudapi",
     packages=["cloudapi"],
-    version="1.0.5",
+    version="1.0.6",
     license="MIT",
     description="BaseRESTAPI ticker, automaticaly manages request response rate limits and timeouts",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/html",
     author="zoran ilievski",
     author_email="pythonic@clientuser.net",
     url="https://github.com/zorani/cloudapi",
-    download_url="https://github.com/zorani/cloudapi/archive/refs/tags/v1.0.5.tar.gz",
+    download_url="https://github.com/zorani/cloudapi/archive/refs/tags/v1.0.6.tar.gz",
     keywords=["baseapi", "requests", "rate limit", "rate limits", "api"],
     install_requires=["requests"],
     classifiers=[
